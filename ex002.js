48a95538e745e0a06136a4f306908366928c017c
krayz <denissilvestre007@gmail.com>
2024-12-20 23:51:14 -0300

    Add files via upload

---
+++ ex002.js
@@ -0,0 +1,11 @@
+const carteira_de_motorista = true;
+const idade = 16;
+
+if (idade >= 18) {
+    if (idade && carteira_de_motorista)
+        console.log ('aprovado!')
+}
+else {
+    console.log ('reprovado')
+    console.log ('carta ou idade invalida!')
+}
